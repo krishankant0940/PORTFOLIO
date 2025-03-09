19f328ad5417fdd7f77d8c95675d6e916ceaeab8
Krishankant sharma <82305655+krishankant0940@users.noreply.github.com>
2025-03-09 07:06:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("viewWorkBtn").addEventListener("click", function () {
+        window.location.href = "projects.html"; // Redirects to projects page
+    });
+});
